17976968b8f1d63564eed66c98766125e54831ab
Elyas Chua-Aziz <cel9@np.edu.sg>
2026-05-28 15:11:56 +0800

    audio fixed

--- Assets/Scripts/CollectibleScript.cs
+++ Assets/Scripts/CollectibleScript.cs
@@ -4,8 +4,27 @@ public class CollectibleScript : MonoBehaviour
 {
     public int collectibleScore = 0; // Store the score value of this collectible, editable from the Unity Inspector. (this allows different collectibles to be worth different amounts of points)
 
+    AudioSource collectibleAudio;
+
+    [SerializeField]
+    AudioClip collectibleAudioClip;
+
+    void Start()
+    {
+        collectibleAudio = GetComponent<AudioSource>(); // Get the AudioSource component attached to this GameObject so we can play a sound when collected
+    }
+
     public void Collect() // Custom method to handle the collection of this item, called from the PlayerScript when the player interacts with it
     {
-        Destroy(gameObject);
+        // if(collectibleAudio != null) // Check if the AudioSource component exists before trying to play a sound
+        // {
+        //     collectibleAudio.Play(); // Play the collection sound effect to provide feedback to the player
+        // }
+        // SOLUTION 1
+        //Destroy(gameObject, collectibleAudio.clip.length); // Destroy this GameObject after the sound has finished playing to remove it from the scene
+    
+        // SOLUTION 2
+        AudioSource.PlayClipAtPoint(collectibleAudioClip, transform.position);
+        Destroy(gameObject); // Immediately destroy this GameObject to remove it from the scene, allowing the sound to play independently
     }
 }
